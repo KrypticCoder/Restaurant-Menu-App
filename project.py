f5584b8c47ff564b1666d2de12c6610c7c57563c
Dimitar <dimdimvas@yahoo.com>
2016-12-22 14:23:37 -0800

    Edit and Delete functionality added

--- project.py
+++ project.py
@@ -31,13 +31,26 @@ def newMenuItem(restaurant_id):
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return 'page to edit new menu item'
+    editItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        editItem.name = request.form['name']
+        session.add(editItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('editmenuitem.html', item=editItem)
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return 'page to delete new menu item'
+    deleteItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        session.delete(deleteItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('deletemenuitem.html', item=deleteItem)
 
 
 # Server only runs if script is executed directly from Python interpreter 
