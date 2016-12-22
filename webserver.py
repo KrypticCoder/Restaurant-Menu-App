138c67a7d2f1e468abedf1af93cfb63fd7b10754
Dimitar <dimdimvas@yahoo.com>
2016-12-22 00:00:25 -0800

    Interactive web application now allows CRUD operations for our restaurantmenu database
    The homepage is located at localhost:8080/restaurants which lists all of the restaurants in the database. On the homepage, there is a link to create a new restaurant that will redirect you to the link 'localhost:8080/restaurants/new'
    and allow you to specify a new restaurant name which is added to the db. Underneath
    each restaurant on the homepage there is a link to 'Edit' the name of a restaurant, that will redirect you to 'localhost:8080/restaurants/[id]/edit' which will find the name of the restaurant you want to edit and update the db with the new name. Beneath each edit link there is a 'Delete' link that brings you to 'localhost:8080/restaurants/[id]/delete' and likewise gives you the option to delete the restaurant from the db or cancel the action.
    
    The program specifies that the 'database_setup' file is inside the IntroToSQLAlchemy folder.This is intended to maintain folder hierarchy on Github and keep each lesson separate. Python only allows importing of files from the current directory. However, we can work around this by appending the subdirectory to the current path and giving Python access to the files within. The database is also inside this folder and its name must be hardcoded as '/IntroToSQLAlchemy/restaurantmenu.db'.

--- webserver.py
+++ webserver.py
@@ -6,49 +6,150 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 # A CGI (Common Gateway Interface) script is invoked by HTTP server to process user input 
 # submited through HTML <FORM> or <ISINDEX> element. A cgi provides an easier way to handle data 
 # from server to client rather than the 'query string' that is part of the URL
-import cgi 
+import cgi
+
+# Need to append the subdirectory to our current path in order to access our previous db files
+# Python only checks up a directory tree, not down 
+import sys
+sys.path.append("./IntroToSQLAlchemy/")
+
+# import the database we created in first lesson
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Create session and connect to DB ##
+engine = create_engine('sqlite:///IntroToSQLALchemy/restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
-            if self.path.endswith('/hello'):
-                self.send_response(200)     # Successful GET request
-
+            #########################################
+            # List all of the restaurants in the db #
+            #########################################
+            if self.path.endswith("/restaurants"):
+                
+                # Successful GET request
+                self.send_response(200)
                 # Inform client we are replying with text in the form of html
-                self.send_header('Content-type', 'text/html')  
+                self.send_header('Content-type', 'text/html')
                 self.end_headers()
 
+                # Get all restaurants stored in db
+                restaurants = session.query(Restaurant).all()
+                
                 output = ""
-                output += "<html><body>"
-                output += "Hello!"
-
-                # Refer to ***
-                output += "<form method='POST' enctype='multipart/form-data' action='/hello'> \
-                            <h2>What would you like me to say?</h2><input name='message' type='text'> \
-                            <input type='submit' value='Submit'> </form>"
-                output += "</body></html>"
-                self.wfile.write(output)
-                print output
-                return
-
-            if self.path.endswith('/hola'):
+                # Provide link to create a new restaurant
+                output += "<a href = '/restaurants/new' > Make a New Restaurant Here </a></br></br>"
+                # Successful GET request
                 self.send_response(200)     
+                # Inform client we are replying with text in the form of html
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+            
 
-                self.send_header('Content-type', 'text/html')  
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "</br>"
+
+                    # Provide link to edit each restaurant listed
+                    output += "<a href ='/restaurants/%s/edit' >Edit </a> " % restaurant.id
+                    output += "</br>"
+
+                    # Provide link to delete each restaurant listed
+                    output += "<a href ='/restaurants/%s/delete'> Delete </a>" % restaurant.id
+                    output += "</br></br></br>"
+
+                output += "</body></html>"
+                self.wfile.write(output)
+                return
+
+            ###########################
+            # Create a new restaurant #
+            ###########################
+            if self.path.endswith("/restaurants/new"):
+
+                # Successful GET request
+                self.send_response(200)
+                # Inform client we are replying with text in the form of html
+                self.send_header('Content-type', 'text/html')
                 self.end_headers()
 
                 output = ""
                 output += "<html><body>"
-                output += "&#161Hola! <a href = '/hello'>Back to Hello</a>"
-                # Refer to ***
-                output += "<form method='POST' enctype='multipart/form-data' action='/hello'> \
-                            <h2>What would you like me to say?</h2><input name='message' type='text'> \
-                            <input type='submit' value='Submit'> </form>"
-                output += "</body></html>"
-                output += "</body></html>"
+                output += "<h1>Make a New Restaurant</h1>"
+                output += "<form method='POST' enctype='multipart/form-data' action ='/restaurants/new' >"
+                output += "<input name='newRestaurantName' type='text' placeholder='New Restaurant Name' > "
+                output += "<input type='submit' value='Create'>"
+                output += "</form></html></body>"
                 self.wfile.write(output)
-                print output
                 return
+
+            ###############################
+            # Edit an existing restaurant #
+            ###############################
+            if self.path.endswith("/edit"):
+
+                # Get the restaurant id from the URL: localhost:8080/restaurant/id/edit
+                editRestaurantID = self.path.split("/")[2]
+                print('edit restaurant id ' + str(editRestaurantID))
+
+                # Find the restaurant we want to edit
+                editRestaurant = session.query(Restaurant).filter_by(id=editRestaurantID).one()
+
+                print('edit restaurant ' + str(editRestaurant.name))
+                
+                # if restaurant exists
+                if editRestaurant:
+                    print('found restaurant')
+                    # Successful GET request
+                    self.send_response(200)
+                    # Inform client we are replying with text in the form of html
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    print('render html')
+                    output = "<html><body>"
+                    output += "<h1>"
+                    output += editRestaurant.name
+                    output += "</h1>"
+                    output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/%s/edit' >" % editRestaurantID
+                    output += "<input name='newRestaurantName' type='text' placeholder='%s' >" % editRestaurant.name
+                    output += "<input type='submit' value='Rename'>"
+                    output += "</form>"
+                    output += "</body></html>"
+                    self.wfile.write(output)
+
+            #################################                             
+            # Delete an existing restaurant # 
+            #################################
+            if self.path.endswith("/delete"):
+
+                # Get the restaurant id from the URL: localhost:8080/restaurant/id/delete
+                deleteRestaurantID = self.path.split("/")[2]
+
+                # Find the restaurant we want to delete
+                deleteRestaurant = session.query(Restaurant).filter_by(id=deleteRestaurantID).one()
+                
+                # If restaurant exists
+                if deleteRestaurant:
+
+                    # Successful GET request
+                    self.send_response(200)
+                    # Inform client we are replying with text in the form of html
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+
+                    output = ""
+                    output += "<html><body>"
+                    output += "<h1>Are you sure you want to delete %s?" % deleteRestaurant.name
+                    output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/%s/delete'>" % deleteRestaurantID
+                    output += "<input type='submit' value='Delete'>"
+                    output += "</form>"
+                    output += "</body></html>"
+                    self.wfile.write(output)
 
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path) # GET request results in error
@@ -56,36 +157,96 @@ class webserverHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            self.send_response(301)  # Used for permanent redirection
-            self.end_headers()
+            ###################################
+            # POST request for new restaurant #
+            ###################################
+            if self.path.endswith("/restaurants/new"):
 
-            # parse_header() parses HTML form header, such as content-type into main value and dictionary of parameters
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                # HTML form header, content-type, is parsed into main value & dictionary of parameters
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
 
-            # Check to see if this is form data being received 
-            if ctype == 'multipart/form-data':
+                # Check to see if main value is form data being received
+                if ctype == 'multipart/form-data':
 
-                # Collect all of the fields in a form
-                fields = cgi.parse_multipart(self.rfile, pdict)
+                    # Collect all of the fields in a form
+                    fields = cgi.parse_multipart(self.rfile, pdict)
 
-                # Get out the value of a specific field(s) and store it/them in an array
-                messagecontent = fields.get('message')
+                    # Take the value of a specific field(s) and store it in an array
+                    messagecontent = fields.get('newRestaurantName')
 
-                output = ""
-                output += "<html><body>"
-                output += "<h2> Okay, how about this: </h2>"
-                output += "<h1> %s </h1>" % messagecontent[0]
+                    # Create new Restaurant object
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
 
-                #######  ***  #######
-                # Add post request along with header tag to prompt user to input data 
-                # Note: input field coincides with the message field we are extracting data from in the POST request
-                output += "<form method='POST' enctype='multipart/form-data' action='/hello'> \
-                            <h2>What would you like me to say?</h2><input name='message' type='text'> \
-                            <input type='submit' value='Submit'> </form>"
-                output += "</body><html>"
-                self.wfile.write(output)
-                print output
+                    # Successful POST request
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
 
+                    # redirect back to homepage
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+
+            #########################################
+            # POST request for editing a restaurant #
+            #########################################
+            if self.path.endswith("/edit"):
+
+                # form header split into main value & dictionary of parameters
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+
+                # Verify we are receiving form data
+                if ctype == 'multipart/form-data':
+
+                    # Collect all fields in form
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+
+                    # Store value of field in array
+                    messagecontent = fields.get('newRestaurantName')
+
+                    # Find the ID of the restaurant we chose to edit
+                    editRestaurantID = self.path.split("/")[2]
+
+                    # Use that ID to find the restaurant
+                    editRestaurant = session.query(Restaurant).filter_by(id=editRestaurantID).one()
+                    
+                    # if restaurant exists
+                    if editRestaurant:
+                        editRestaurant.name = messagecontent[0]
+                        session.add(editRestaurant)
+                        session.commit()
+
+                        # Successful POST request
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+
+                        # Redirect back to home page
+                        self.send_header('Location', '/restaurants')
+                        self.end_headers()
+
+            ##########################################
+            # POST request for deleting a restaurant #
+            ##########################################
+            if self.path.endswith("/delete"):
+
+                # Find the id of the restaurant we chose to delete
+                deleteRestaurantID = self.path.split("/")[2]
+
+                # Use that id to find the restaurant
+                deleteRestaurant = session.query(Restaurant).filter_by(id=deleteRestaurantID).one()
+                
+                # if restaurant exists
+                if deleteRestaurant:
+                    session.delete(deleteRestaurant)
+                    session.commit()
+
+                    # Successful POST request
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+
+                    # Redirect back to home page
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
         except:
             pass
 
@@ -96,7 +257,7 @@ def main():
         # HTTPServer(server_address, RequestHandlerClass)
         # server_address contains host (empty string), and port number (8080)
         server = HTTPServer(('', port), webserverHandler)
-        print "Web server running on port %s" % port
+        print "Web server running ... open localhost:%s/restaurants in your browser" % str(port)
         server.serve_forever()
 
     except KeyboardInterrupt:
